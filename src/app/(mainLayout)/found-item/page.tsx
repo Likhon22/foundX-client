bc8f6b84ed5df1ed82b0331c85147eeffa90e5bb
Likhon22 <likhonsarker793@gmail.com>
2025-03-25 06:23:34 +0600

    initial commit

---
+++ src/app/(mainLayout)/found-item/page.tsx
@@ -0,0 +1,9 @@
+const FoundItem = () => {
+  return (
+    <div>
+      <p>Found Item</p>
+    </div>
+  );
+};
+
+export default FoundItem;
---
+++ src/config/site.ts
@@ -0,0 +1,40 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + HeroUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Found Item",
+      href: "/found-item",
+    },
+    {
+      label: "register",
+      href: "/register",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Found Item",
+      href: "/found-item",
+    },
+  ],
+};
