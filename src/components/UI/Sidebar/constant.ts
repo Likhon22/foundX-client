70a98a48934279216fdbba8516e8be7cf21f1afc
Likhon22 <likhonsarker793@gmail.com>
2025-03-29 21:11:28 +0600

    feat:added sidebar

---
+++ src/components/UI/Sidebar/constant.ts
@@ -0,0 +1,6 @@
+export const userLinks = [
+  { href: "profile/create-post", label: "Posts" },
+  { href: "profile/setting", label: "Settings" },
+];
+
+export const adminLinks = [{ href: "/admin", label: "Admin" }];
